65ec3abe04e2738f9b802f172da9e9f755dd0a39
cae223 <cgonyea@massart.edu>
2022-03-03 02:47:07 -0500

    copy and paste is a girl's best friend btw (scene 12 %99 done)

--- MA_SP22_story4/Assets/Scripts/Scene12_Dialogue.cs
+++ MA_SP22_story4/Assets/Scripts/Scene12_Dialogue.cs
@@ -14,9 +14,9 @@ public class Scene12_Dialogue : MonoBehaviour {
       //  public Text Char3name;
       //  public Text Char3speech;
         public GameObject dialogue;
-        public GameObject ArtYogAngry1;
-        public GameObject ArtYogAngry2;
-        public GameObject ArtYogMonster;
+        public GameObject ArtNar1;
+        public GameObject ArtNar2;
+        public GameObject ArtKthulu;
         public GameObject ArtBG1;
         public GameObject Choice1a;
         public GameObject Choice1b;
@@ -29,9 +29,9 @@ public class Scene12_Dialogue : MonoBehaviour {
 
 void Start(){         // initial visibility settings
         dialogue.SetActive(false);
-        ArtYogAngry1.SetActive(false);
-        ArtYogAngry2.SetActive(false);
-        ArtYogMonster.SetActive(false);
+        ArtNar1.SetActive(false);
+        ArtNar2.SetActive(false);
+        ArtKthulu.SetActive(false);
         ArtBG1.SetActive(true);
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
@@ -55,77 +55,107 @@ public void talking(){         // main story function. Players hit next to progr
                 // AudioSource.Play();
         }
         else if (primeInt == 2){
-
                 dialogue.SetActive(true);
                 Char1name.text = "YOU";
-                Char1speech.text = "Could you erase the runes? You can’t draw all over the desks.";
+                Char1speech.text = "[Principal Kthulu is here to help! Finally!]";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt ==3){
-         ArtYogAngry1.SetActive(true);
+         ArtKthulu.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "YOG";
-                Char2speech.text = "You want me to get rid of my tribute?";
+                Char2name.text = "KTHULU";
+                Char2speech.text = "What's the issue here?";
                 //gameHandler.AddPlayerStat(1);
         }
        else if (primeInt == 4){
-                Char1name.text = "YOU";
-                Char1speech.text = "Please. It’s against the rules.";
-                Char2name.text = "";
-                Char2speech.text = "";
-        }
-       else if (primeInt == 5){
+                ArtNar1.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "YOG";
-                Char2speech.text = "The Oldest Ones deserve their tribute! You can’t ask me to destroy it!";
+                Char2name.text = "NAR";
+                Char2speech.text = "The human’s trying to stop me from doing a summon.";
+        }
+       else if (primeInt == 5){
+                Char1name.text = "YOU";
+                Char1speech.text = " He’s refusing to stop! It’s putting everyone in danger, summoning a demon for some food!";
+                Char2name.text = "";
+                Char2speech.text = "";
                 //gameHandler.AddPlayerStat(1);
         }
        else if (primeInt == 6){
-                Char1name.text = "YOU";
-                Char1speech.text = "I don’t mean to be rude! It’s just that they’re really not the best way to pay tribute!";
-                Char2name.text = "";
-                Char2speech.text = "";
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "NAR";
+                Char2speech.text = "You’re not used to this school at all, wow.";
         }
        else if (primeInt ==7){
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "YOG";
-                Char2speech.text = "You’re not the best way to be a teacher!";
+                Char2name.text = "KTHULU";
+                Char2speech.text = "Nyarlathotep is right. You don’t understand our school";
         }
         else if (primeInt == 8){
                  Char1name.text = "YOU";
-                 Char1speech.text = "[Yog has gotten a lot bigger!]";
+                 Char1speech.text = "What?";
                  Char2name.text = "";
                  Char2speech.text = "";
          }
         else if (primeInt ==9){
-                 Char1name.text = "YOU";
-                 Char1speech.text = "[And scarier. I start to cower, just a little bit.]";
-                 Char2name.text = "";
-                 Char2speech.text = "";
+                 Char1name.text = "";
+                 Char1speech.text = "";
+                 Char2name.text = "KTHULU";
+                 Char2speech.text = "You dare interrupt a summoning at any time? These students are far and away better than any old human who enters this building.";
          }
          else if (primeInt ==10){
                   Char1name.text = "YOU";
-                  Char1speech.text = "Um. I’m sorry.";
+                  Char1speech.text = "Then why not explain that in the first place instead of just saying that you won’t stop?";
                   Char2name.text = "";
                   Char2speech.text = "";
           }
           else if (primeInt ==11){
                    Char1name.text = "";
                    Char1speech.text = "";
-                   Char2name.text = "YOG";
-                   Char2speech.text = "You better be sorry! My tributes are the best! You need to step back, you’re just some substitute!";
+                   Char2name.text = "NAR";
+                   Char2speech.text = "That's easy.";
            }
            else if (primeInt ==12){
+                    Char1name.text = "";
+                    Char1speech.text = "";
+                    Char2name.text = "NAR";
+                    Char2speech.text = "You're just some human.";
+          }
+          else if (primeInt ==13){
+                   Char1name.text = "YOU";
+                   Char1speech.text = "[This is the worst school I’ve ever substituted at. I'm never ever coming back.]";
+                   Char2name.text = "";
+                   Char2speech.text = "";
+           }
+           else if (primeInt ==14){
+                    Char1name.text = "";
+                    Char1speech.text = "";
+                    Char2name.text = "KTHULU";
+                    Char2speech.text = "Nyarlethotep, begin your summon.";
+            }
+            else if (primeInt ==15){
+                     Char1name.text = "";
+                     Char1speech.text = "";
+                     Char2name.text = "KTHULU";
+                     Char2speech.text = "We have a sacrifice to make.";
+             }
+           else if (primeInt ==16){
                     Char1name.text = "YOU";
-                    Char1speech.text = "[This is the worst first day I’ve ever had. If you somehow manage to survive this, I'm never subbing for this school again.]";
+                    Char1speech.text = "Huh?";
                     Char2name.text = "";
                     Char2speech.text = "";
           }
-         else if (primeInt == 13){
+             else if (primeInt ==17){
+                      Char1name.text = "";
+                      Char1speech.text = "";
+                      Char2name.text = "KTHULU";
+                      Char2speech.text = "You're being sacrificed, human.";
+              }
+         else if (primeInt == 18){
                 Char1name.text = "YOU";
                 Char1speech.text = "";
                 Char2name.text = "";
@@ -137,18 +167,35 @@ public void talking(){         // main story function. Players hit next to progr
                 Choice1b.SetActive(true); // function Choice1bFunct()
         }
 // ENCOUNTER AFTER CHOICE #1
-       else if (primeInt == 100){
-                Char1name.text = "YOU";
-                Char1speech.text = "Sorry I asked! It’s just that there has to be a better way!";
-                Char2name.text = "";
-                Char2speech.text = "";
+        else if (primeInt ==100){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "NAR";
+                Char2speech.text = "Anyway, let me grab my camera. It's vital to the summon.";
+        }
+       else if (primeInt == 101){
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "KTHULU";
+                Char2speech.text = "I doubt that. Besides, we don't need another video of a human getting devoured by a delivery demon on the internet.";
                 primeInt = 299;
         }
 
-
-       else if (primeInt == 200){
+        else if (primeInt ==200){
+                 Char1name.text = "";
+                 Char1speech.text = "";
+                 Char2name.text = "NAR";
+                 Char2speech.text = "Can I film this go down?";
+         }
+         else if (primeInt ==201){
+                  Char1name.text = "";
+                  Char1speech.text = "";
+                  Char2name.text = "KTHULU";
+                  Char2speech.text = "I don't see why not. Humiliation sometimes aids in a lesson.";
+          }
+       else if (primeInt == 202){
                 Char1name.text = "YOU";
-                Char1speech.text = "It’s just some runes! It can’t be that hard to just use paper!";
+                Char1speech.text = "It's literally a Monday! You don't need to do this!";
                 Char2name.text = "";
                 Char2speech.text = "";
                 primeInt = 299;
@@ -156,40 +203,28 @@ public void talking(){         // main story function. Players hit next to progr
 
 
         else if (primeInt == 300){
-                 Char1name.text = "";
+                 Char1name.text = "YOU";
                  Char1speech.text = "";
-                 Char2name.text = "YOG";
-                 Char2speech.text = "AAAAAAARRRGGGGHHH!!!!";
+                 Char2name.text = "";
+                 Char2speech.text = "[Nar rumbles out a language I just can't wrap my head around. In fact, my head hurts from just hearing it.]";
          }
          else if (primeInt == 301){
                   Char1name.text = "YOU";
-                  Char1speech.text = "[I’ve screwed up.]";
+                  Char1speech.text = "[Something crawls out of the portal holding a paper bag that smells pretty good. Nar dishes out the necessary cash, and then I get pushed in.]";
                   Char2name.text = "";
                   Char2speech.text = "";
-          }
-          else if (primeInt == 302){
-                   Char1name.text = "YOU";
-                   Char1speech.text = "[Yog’s power starts to come off of him in waves, and I'm clearly the target.]";
-                   Char2name.text = "";
-                   Char2speech.text = "";
-           }
-           else if (primeInt == 303){
-                    Char1name.text = "YOU";
-                    Char1speech.text = "[I have to get behind the desk!]";
-                    Char2name.text = "";
-                    Char2speech.text = "";
+        }
+           else if (primeInt == 302){
+                    Char1name.text = "";
+                    Char1speech.text = "";
+                    Char2name.text = "KTHULU";
+                    Char2speech.text = "Sorry, human. This is just how it is at this school. Learn some respect for summons.";
             }
-            else if (primeInt == 303){
-                     Char1name.text = "YOU";
-                     Char1speech.text = "[I have to get behind the desk!]";
-                     Char2name.text = "";
-                     Char2speech.text = "";
-             }
-       else if (primeInt == 304){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "YOG";
-                Char2speech.text = "You are pathetic and idiotic, substitute! I will smite you where you stand for disrespecting the Oldest Ones!";
+       else if (primeInt == 303){
+                Char1name.text = "YOU";
+                Char1speech.text = "It was just some food delivery!!";
+                Char2name.text = "";
+                Char2speech.text = "";
                 nextButton.SetActive(false);
                 allowSpace = false;
                 NextScene1Button.SetActive(true);
@@ -200,8 +235,8 @@ public void talking(){         // main story function. Players hit next to progr
         public void Choice1aFunct(){
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "YOG";
-                Char2speech.text = "There is no middle ground! You have insulted me by suggesting I erase them in the first place!";
+                Char2name.text = "KTHULU";
+                Char2speech.text = "I would hope this is worse.";
                 primeInt = 99;
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
@@ -211,8 +246,8 @@ public void talking(){         // main story function. Players hit next to progr
         public void Choice1bFunct(){
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "YOG";
-                Char2speech.text = "DO YOU EVEN VALUE YOUR OWN LIFE, HUMAN?";
+                Char2name.text = "Kthulu";
+                Char2speech.text = "It absolutely is necessary! You need to be taught a lesson!";
                 primeInt = 199;
                 Choice1a.SetActive(false);
                 Choice1b.SetActive(false);
@@ -221,7 +256,7 @@ public void talking(){         // main story function. Players hit next to progr
         }
 
         public void SceneChange1(){
-               SceneManager.LoadScene("Scene13");
+               SceneManager.LoadScene("Scene15");
         }
       //  public void SceneChange2(){
             //    SceneManager.LoadScene("Scene2b");
