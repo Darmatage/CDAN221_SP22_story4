2e0765852875b8e301ba0d11654c86b130c41980
VanAnson <jadodds@me.com>
2022-03-22 23:15:44 -0400

    CHA CHA Changes made to Scenes 7-9

--- MA_SP22_story4/Assets/Scripts/Scene9_dialogue.cs
+++ MA_SP22_story4/Assets/Scripts/Scene9_dialogue.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
-public class Scene7_dialogue : MonoBehaviour {
+public class Scene9_dialogue : MonoBehaviour {
         public int primeInt = 1;         // This integer drives game progress!
         public Text Char1name;
         public Text Char1speech;
         public Text Char2name;
         public Text Char2speech;
         public GameObject DialogueDisplay;
-        public GameObject ArtNar1;
+        public GameObject ArtYog1;
         public GameObject ArtBG1;
         public GameObject NextScene1Button;
         public GameObject nextButton;
@@ -20,7 +20,7 @@ public class Scene7_dialogue : MonoBehaviour {
 
 void Start(){         // initial visibility settings
         DialogueDisplay.SetActive(false);
-        ArtNar1.SetActive(false);
+        ArtYog1.SetActive(false);
         ArtBG1.SetActive(true);
       //  Choice1a.SetActive(false);
       //  Choice1b.SetActive(false);
@@ -43,76 +43,77 @@ public void talking(){         // main story function. Players hit next to progr
                 // AudioSource.Play();
         }
         else if (primeInt == 2){
-               ArtNar1.SetActive(false);
+               ArtYog1.SetActive(false);
                 DialogueDisplay.SetActive(true);
                 Char1name.text = "You";
-                Char1speech.text = "Another successful student interaction. To teach really IS to change lives forever, huh? ";
+                Char1speech.text = "Well, maybe I could help you!";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt == 3){
-         ArtNar1.SetActive(true);
+         ArtYog1.SetActive(true);
           DialogueDisplay.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Nyarlathotep";
-                Char2speech.text = "[filming self] Hey Guys! It’s Nar coming to you live with the YouCraft video to top all YouCraft videos!";
+                Char2name.text = "Yog-Sothoth";
+                Char2speech.text = "What does a human like you know about crafting runes?";
                 //gameHandler.AddPlayerStat(1);
         }
+
        else if (primeInt == 4){
-         ArtNar1.SetActive(true);
+         ArtYog1.SetActive(true);
           DialogueDisplay.SetActive(true);
                 Char1name.text = "You";
-                Char1speech.text = "Wait, what’s going on over there?";
+                Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt == 5){
-         ArtNar1.SetActive(true);
+         ArtYog1.SetActive(true);
           DialogueDisplay.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Nyarlathotep";
-                Char2speech.text = "My sick summoning ritual is almost complete!";
+                Char2name.text = "";
+                Char2speech.text = "";
                 //gameHandler.AddPlayerStat(1);
         }
        else if (primeInt == 6){
-         ArtNar1.SetActive(true);
+        ArtYog1.SetActive(true);
           DialogueDisplay.SetActive(true);
                 Char1name.text = "You";
-                Char1speech.text = "Wha- what?";
+                Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
        else if (primeInt ==7){
-         ArtNar1.SetActive(true);
+         ArtYog1.SetActive(true);
           DialogueDisplay.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Nyarlathotep";
-                Char2speech.text = "[pausing from filming] Ugh. Is it, like, your first day in a school ever? I’m obviously opening a portal for a delivery demon to bring the food I ordered for lunch through.";
+                Char2name.text = "";
+                Char2speech.text = "";
         }
        else if (primeInt == 8){
-         ArtNar1.SetActive(true);
+         ArtYog1.SetActive(true);
           DialogueDisplay.SetActive(true);
                 Char1name.text = "You";
-                Char1speech.text = "What?! Cut that out immediately!";
+                Char1speech.text = "";
                 Char2name.text = "";
                 Char2speech.text = "";
 }
                 else if (primeInt == 9){
-                  ArtNar1.SetActive(true);
+                  ArtYog1.SetActive(true);
                    DialogueDisplay.SetActive(true);
                          Char1name.text = "";
                          Char1speech.text = "";
-                         Char2name.text = "Nyarlathotep";
-                         Char2speech.text = "[filming again] Like and subscribe if you want to see me push my annoying substitute teacher through the portal as a sick bonus prank!";
+                         Char2name.text = "";
+                         Char2speech.text = "";
 }
                          else if (primeInt == 10){
-                           ArtNar1.SetActive(true);
+                           ArtYog1.SetActive(true);
                             DialogueDisplay.SetActive(true);
                                   Char1name.text = "You";
-                                  Char1speech.text = "That’s it! I’m calling in Principal Kthulu.";
+                                  Char1speech.text = "";
                                   Char2name.text = "";
                                   Char2speech.text = "";
 }
