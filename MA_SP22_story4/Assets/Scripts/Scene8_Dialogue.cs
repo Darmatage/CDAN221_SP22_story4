0f38137a7372c13b97964cbb53f7afb01c565fdc
emilymarini <emarini@massart.edu>
2022-03-30 22:57:58 -0400

    Scene 8 Better now?

--- MA_SP22_story4/Assets/Scripts/Scene8_Dialogue.cs
+++ MA_SP22_story4/Assets/Scripts/Scene8_Dialogue.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
-public class Scene8_dialogue : MonoBehaviour {
+public class Scene8_Dialogue : MonoBehaviour {
         public int primeInt = 1;         // This integer drives game progress!
         public Text Char1name;
         public Text Char1speech;
